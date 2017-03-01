53383435aaa66fac8a04b184f1d35e8dfd39b891
Jake Miller <hillsighed@gmail.com>
2017-02-28 21:19:16 -0800

    adds lake page

--- routes/index.js
+++ routes/index.js
@@ -19,11 +19,21 @@ router.get('/lakes', (req, res, next) => {
     res.render('index', {title: 'Fish.ly'})
   } else {
     Lake.find({}, function (err, results) {
-      res.render('lakes', {title: 'All Lakes', avatar: req.session.user.image.url, name: req.session.user.name, lakes: results[0].name})
-      console.log(results);
+      res.render('lakes', {title: 'All Lakes', avatar: req.session.user.image.url, name: req.session.user.name, lakes: results[0]})
     })
   }
 })
+
+router.get('/lake/:id', (req, res, next) => {
+  if (!req.session.user) {
+    res.render('index', {title: 'Fish.ly'})
+  } else {
+    Lake.findById({_id: req.params.id}, function (err, results) {
+      res.render('lake', {title: 'All Fish From Lake', avatar: req.session.user.image.url, name: req.session.user.name, lake: results})
+    })
+  }
+})
+
 
 router.get('/fish', (req, res, next) => {
   if (!req.session.user) {
